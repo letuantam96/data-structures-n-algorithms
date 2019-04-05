ccd7ab94ea231d695a68534152fc0e4867f929c7
Tuấn Tâm <39912206+letuantam96@users.noreply.github.com>
2019-04-05 15:15:17 +0700

    Update HYDRO solution.cpp

--- HYDRO solution.cpp
+++ HYDRO solution.cpp
@@ -1,45 +1,33 @@
 #include <iostream>
-using namespace std;
 
-int A[10000], B[10000], n, nB;
-int imax, T;
-
-void cal()
-{
-	for (int i = 0; i < nB - 1; i++)
-		if (B[i] > B[i+1])
-		{
-			T += (B[i] - B[i+1]);
-			B[i+1] = B[i];
-		}
-}
+int TC, n, A[10000], T, imax;
 
 int main()
 {
-	//freopen("input.txt","r",stdin);
-	int TC;
-	cin >> TC;
+	std::cin >> TC;
 	for (int itc = 0; itc < TC; itc++)
 	{
-		imax = 0, T = 0;
-		cin >> n;
+		T = 0; imax = 0;
+		std::cin >> n;
 		for (int i = 0; i < n; i++)
 		{
-			cin >> A[i];
+			std::cin >> A[i];
 			if (A[i] > A[imax]) imax = i;
 		}
 
-		for (int i = 0; i <= imax; i++)
-			B[i] = A[i];
-		nB = imax + 1;
-		cal();
-
-		for (int i = n - 1; i >= imax; i--)
-			B[n - 1 - i] = A[i];
-		nB = n - imax;
-		cal();
-
-		cout << T << endl;
+		for (int i = 0; i <= imax - 1; i++)
+			if (A[i] > A[i+1])
+			{
+				T += A[i] - A[i+1];
+				A[i+1] = A[i];
+			}
+		for (int i = n - 1; i >= imax + 1; i--)
+			if (A[i] > A[i-1])
+			{
+				T += A[i] - A[i-1];
+				A[i-1] = A[i];
+			}
+		std::cout << T << std::endl;
 	}
 	return 0;
 }
